8de2ed6d31d091a263f22e5ae2449305846bb54b
Soner Kuyar <81442313+sonerkuyar@users.noreply.github.com>
2024-04-21 15:04:31 +0300

    Adding Theme and organize file structure

--- app/frontend/playlog/src/App.js
+++ app/frontend/playlog/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Playlog!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=" "
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
+        
         </a>
+        <h1>Playlog</h1>
       </header>
     </div>
   );
