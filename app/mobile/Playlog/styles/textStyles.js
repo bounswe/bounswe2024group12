9a3dd46d7d066721dd10bf899eada6812817c0cb
Taha Ensar <taha.kukul@boun.edu.tr>
2024-04-25 18:10:47 +0300

    Change: Some style changes in TextStyles

--- app/mobile/Playlog/styles/textStyles.js
+++ app/mobile/Playlog/styles/textStyles.js
@@ -4,13 +4,16 @@ export default textStyles = StyleSheet.create({
     default: {
         fontSize: 16,
         color: 'white',
+        lineHeight: 17,
     },
     title: {
         fontSize: 24,
         color: 'white',
+        fontWeight: 'bold',
     },
     cardTitle: {
         fontSize: 20,
         color: 'white',
+        fontWeight: 'bold',
     },
 })
