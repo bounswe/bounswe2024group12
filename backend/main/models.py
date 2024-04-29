460865a0df7cf03ea6985894310f4ab9a8f13f45
ahmetfirat <ahmetfiratgamsiz287@gmail.com>
2024-04-29 21:16:30 +0300

    refactored code yet no good

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,15 +1,34 @@
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.db import models
 from django.contrib.auth.models import User
     
-
-class RegisteredUser(models.Model):
-    # username is unique
+class RegisteredUserManager(BaseUserManager):  
+    def  create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):  
+        return self.create_user(email, username, password, **extra_fields)
+    
+class RegisteredUser(AbstractBaseUser):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-
+    is_active = models.BooleanField(default=True)
+    
+    objects = RegisteredUserManager()
+    
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', 'password']
+    
     def __str__(self):
         return self.username
-    
-    def check_password(self, password):
-        return self.password == password
+
+
