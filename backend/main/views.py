e671a62f7e896a65bf9eff7e9f568d2353a439bd
Soner Kuyar <81442313+sonerkuyar@users.noreply.github.com>
2024-04-30 19:50:19 +0300

    SearchBar features
    It needs help.

--- backend/main/views.py
+++ backend/main/views.py
@@ -8,6 +8,16 @@ from django.contrib.auth import authenticate, login, logout
 import logging
 from django.utils import timezone
 
+def extract_unique_values(results, key):
+                values = set()
+                try:
+                    for item in results['results']['bindings']:
+                        if key in item:
+                            values.add(item[key]['value'])
+                    return values
+                except:
+                    return values
+
 
 @csrf_exempt  # Only for demonstration. CSRF protection should be enabled in production.
 def signup(request):
@@ -73,12 +83,7 @@ def property_game(request):
             }
                 """
         
-        def extract_unique_values(results, key):
-                values = set()
-                for item in results['results']['bindings']:
-                    if key in item:
-                        values.add(item[key]['value'])
-                return values
+        
         url = 'https://query.wikidata.org/sparql'
         headers = {'User-Agent': 'Mozilla/5.0 (Django Application)', 'Accept': 'application/json'}
         response = requests.get(url, headers=headers, params={'query': sparql_query, 'format': 'json'})
@@ -92,6 +97,7 @@ def property_game(request):
                 unique_publishers = extract_unique_values(results, 'publisherLabel')
                 unique_countries = extract_unique_values(results, 'countryLabel')
                 unique_screenwriters = extract_unique_values(results, 'screenwriterLabel')
+                unique_publication_dates = extract_unique_values(results, 'publication_date')
                 unique_composers = extract_unique_values(results, 'composerLabel')
                 unique_platforms = extract_unique_values(results, 'platformLabel')
                 return JsonResponse({
@@ -101,7 +107,9 @@ def property_game(request):
                     'countries': list(unique_countries),
                     'screenwriters': list(unique_screenwriters),
                     'composers': list(unique_composers),
-                    'platforms': list(unique_platforms)
+                    'platforms': list(unique_platforms),
+                    'publication_dates': list(unique_publication_dates)
+
                                 })
             else:
                 return JsonResponse({'error': 'No game found with that name'}, status=404)
@@ -110,6 +118,81 @@ def property_game(request):
 
     return JsonResponse({'error': 'Invalid request method'}, status=405)
 
+@csrf_exempt
+def search_game(request):
+    if request.method == 'POST':
+        print("REQUEST BODY" , request.body)
+        data = json.loads(request.body)  # Parse the JSON data from the request body
+        game_name = data.get('game_name')
+        print("GAME NAME", game_name)
+        if not game_name:
+            return JsonResponse({'error': 'Game name is required'}, status=400)
+        
+        sparql_query = f"""
+                SELECT DISTINCT ?game ?gameLabel ?genreLabel ?publisherLabel ?countryLabel ?publication_date ?logo ?image ?charactersLabel ?screenwriterLabel ?composerLabel
+                WHERE {{
+                    ?game wdt:P31 wd:Q7889.
+                    ?game rdfs:label "{game_name}"@en;
+
+                    OPTIONAL {{ ?game wdt:P136 ?genre. }}
+                    OPTIONAL {{ ?game wdt:P123 ?publisher. }}
+                    OPTIONAL {{ ?game wdt:P495 ?country. }}
+                    OPTIONAL {{ ?game wdt:P577 ?publication_date. }}
+                    OPTIONAL {{ ?game wdt:P58  ?screenwriter. }}
+                    OPTIONAL {{ ?game wdt:P86  ?composer. }}
+                    OPTIONAL {{ ?game wdt:P154 ?logo. }}
+                    OPTIONAL {{ ?game wdt:P18  ?image. }}
+                    OPTIONAL {{ ?game wdt:P674 ?characters. }}
+
+                    SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+                }}
+                LIMIT 40
+            """
+
+        url = 'https://query.wikidata.org/sparql'
+        headers = {'User-Agent': 'Mozilla/5.0 (Django Application)', 'Accept': 'application/json'}
+        response = requests.get(url, headers=headers, params={'query': sparql_query, 'format': 'json'})
+
+        if response.status_code == 200:
+            results = response.json()
+
+            if results:
+                unique_games = extract_unique_values(results, 'gameLabel')
+                unique_genres = extract_unique_values(results, 'genreLabel')
+                unique_publishers = extract_unique_values(results, 'publisherLabel')
+                unique_countries = extract_unique_values(results, 'countryLabel')
+                unique_publication_dates = extract_unique_values(results, 'publication_date')
+                unique_logos = extract_unique_values(results, 'logo')
+                unique_images = extract_unique_values(results, 'image')
+                unique_characters = extract_unique_values(results, 'charactersLabel')
+                unique_screenwriters = extract_unique_values(results, 'screenwriterLabel')
+                unique_composers = extract_unique_values(results, 'composerLabel')
+
+
+
+                
+                return JsonResponse({
+                    'games': list(unique_games),
+                    'genres': list(unique_genres),
+                    'publishers': list(unique_publishers),
+                    'countries': list(unique_countries),
+                    'publication_dates': list(unique_publication_dates),
+                    'logos': list(unique_logos),
+                    'images': list(unique_images),
+                    'characters': list(unique_characters),
+                    'screenwriters': list(unique_screenwriters),
+                    'composers': list(unique_composers)
+                    
+                
+                    
+
+                                })
+            else:
+                return JsonResponse({'error': 'No game found with that name'}, status=404)
+        else:
+            return JsonResponse({'error': 'Failed to query Wikidata'}, status=response.status_code)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 def index(request):
     return JsonResponse({'message': 'Welcome to the PlayLog API!'})
