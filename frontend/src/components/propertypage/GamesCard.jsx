7d846422fb95c14facbba41f9ccca5e2de119fa0
ahmetfirat <ahmetfiratgamsiz287@gmail.com>
2024-05-10 19:15:18 +0300

    placeholder, game navigation and media css for low width

--- frontend/src/components/propertypage/GamesCard.jsx
+++ frontend/src/components/propertypage/GamesCard.jsx
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import style from './GamesCard.module.css';
+import { useNavigate } from 'react-router-dom';
+import gamePlaceholder from '../../assets/game_placeholder.png'
 
 export default function GamesCard({data}){
     const [gamesData, setGamesData] = useState([...data]);
+    const navigate = useNavigate();
+
+    function handleClick(game){
+        console.log(game.name)
+        // replace spaces with dashes
+        const name = game.name.replace(/\s/g, '_');
+        navigate(`/game/${name}`);
+    }
     
     useEffect(() => {
         setGamesData(data);
@@ -12,15 +22,14 @@ export default function GamesCard({data}){
         <div className={style.GamesBlock}>
             {gamesData.map((game) => {
                 return (
-                    <div className={style.GameBlock}>
-                        <img width="200px" src={game.image} alt={game.name} />
+                    <div className={style.GameBlock} onClick={()=>{handleClick(game);}}>
+                        <img width="200px" src={(game.image!=="") ? game.image : gamePlaceholder} alt={game.name} />
                         <div className={style.GameInfoBlock}>
                         <div className={style.GameTitleBlock}>
                         <h1>{game.name}</h1>
                         <h3>Rating: {game.rating} / 5</h3>
                         </div>
-                        <h4>{game.genre}</h4>
-                        
+                        <h4>{game.genre!=="" ? game.genre : "genre unknown"}</h4>
                         <p>{game.description}</p>            
                         </div>
                     </div>
