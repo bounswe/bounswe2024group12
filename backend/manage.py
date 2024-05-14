a9b64cac34dc4a52b4d2a1213db93477a87cc847
ahmetfirat <ahmetfiratgamsiz287@gmail.com>
2024-05-14 11:27:08 +0300

    converted urls to environment variables

--- backend/manage.py
+++ backend/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command as runserver
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    runserver.default_port = os.getenv("BACKEEND_PORT", "8000")
     execute_from_command_line(sys.argv)
 
 
