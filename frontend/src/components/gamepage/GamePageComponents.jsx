82acf8a842f45cac1c1195f8609348124e5bbe45
Abstracted <asyasusen@gmail.com>
2024-05-15 18:46:43 +0300

    structure of game page complete

--- frontend/src/components/gamepage/GamePageComponents.jsx
+++ frontend/src/components/gamepage/GamePageComponents.jsx
@@ -6,7 +6,8 @@ import styles from './GamePageComponents.module.css';
 import GameInfo from './GameInfo';
 import TabComponent from "./TabComponent";
 
-export default function GamePageComponents(){
+export default function GamePageComponents({game}){
+  //todo fill here when isil returns
   const [loggedIn, setLoggedIn] = useState(true);
   return (
     <>
