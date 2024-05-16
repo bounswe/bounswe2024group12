c6863e8ac431276e2e8e3b19142ff617531401e8
Isil Su Karakuzu <isilsukarakuzu1967@gmail.com>
2024-05-16 18:32:54 +0300

    feat: added game functionality and database

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,13 +21,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('', views.index, name='index'),
-    path('search-game', views.search_game, name='search-game'),
-    path('game-of-the-day', views.get_game_of_the_day, name='game-of-the-day'),
-    path('all-games', views.get_all_games, name='all-games'),
-    path('popular-games', views.get_popular_games, name='popular-games'),
-    path('new-games', views.get_new_games, name='new-games'),
-    path('game-slug', views.get_game_slug, name='game-slug'),
-    path('game-info/<str:game_slug>/', views.get_game_info, name='game_info'),
     ]
 
 
