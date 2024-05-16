dbbee0caffaa819f8ec6e1eb8de1192b6d5cad3b
ahmetfirat <ahmetfiratgamsiz287@gmail.com>
2024-05-16 17:33:07 +0300

    fixed directory mistake

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,7 +1,7 @@
 import GamePageComponents from "../components/gamepage/GamePageComponents";
 import { useParams } from "react-router-dom";
 import React,{useState, useEffect} from 'react';
-import { endpoint } from '../common/EndpointContext';
+import { endpoint } from '../components/common/EndpointContext';
 
 export default function GamePage() {
     let { id } = useParams();
