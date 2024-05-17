9611c89b782f6a1bb82d8e9019d5331b90a9caf7
Taha Ensar <taha.kukul@boun.edu.tr>
2024-05-17 04:52:38 +0300

    SearchBar was implemented

--- app/mobile/Playlog/components/main/MainScreenComponents.js
+++ app/mobile/Playlog/components/main/MainScreenComponents.js
@@ -6,23 +6,75 @@ import GameListCard from "../commons/GameListCard";
 import MoreGamesGrid from "./MoreGamesGrid";
 import ReviewListCard from "./ReviewListCard";
 import MainPageBanner from "./MainPageBanner";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ProfileContext } from "../../context/ProfileProvider";
 import SearchBar from "../commons/SearchBar";
+import textStyles from "../../styles/textStyles";
+import TextButton from "../buttons/TextButton";
+import { useHttp } from "../../hooks/useHttp";
+
+
+const makeCancelable = (promise) => {
+    let hasCanceled_ = false;
+
+    const wrappedPromise = new Promise((resolve, reject) => {
+        promise.then(
+            val => hasCanceled_ ? reject({ isCanceled: true }) : resolve(val),
+            error => hasCanceled_ ? reject({ isCanceled: true }) : reject(error)
+        );
+    });
+
+    return {
+        promise: wrappedPromise,
+        cancel() {
+            hasCanceled_ = true;
+        },
+    };
+}
 
 export default MainScreenComponents = () => {
     const [isSearch, setIsSearch] = useState(false)
     const { username, token, isGuest, logoutHandler } = useContext(ProfileContext)
-
+    const [searchFetch, setSearchFetch] = useState(null)
     const navigation = useNavigation();
+    const searchRef = useRef(null);
+    const {sendRequest,isLoading,error,clearError} = useHttp()
+
+    useEffect(() => {
+        return () => {
+            if (searchRef.current) {
+                searchRef.current.cancel();
+            }
+        }
+    }, [searchFetch])
 
     const onLogout = () => {
         logoutHandler();
         navigation.replace('Login');
     }
 
+
     const onSearch = (query) => {
-        console.log(query);
+        //TODO: handle previous search abortion
+        if (query) {
+            console.log(process.env.EXPO_PUBLIC_URL)
+            const fetchPromise = sendRequest(`${process.env.EXPO_PUBLIC_URL}/search-game`,
+            'POST',
+            JSON.stringify({game_name: query}),
+            {
+                'Content-Type': 'application/json',
+            }).then((response) => {               
+                setSearchFetch(response)
+                console.log(response)
+            }, (error) => {
+                if (!error.isCanceled) {
+                console.log("not canceled")
+                console.error(error);
+            }else{
+                console.log("canceled")
+            }
+        });
+        }
     }
 
     const onFocus = () => { 
@@ -36,7 +88,7 @@ export default MainScreenComponents = () => {
     return (
         <ScrollView contentContainerStyle={styles.scrollViewContainer} style={styles.scrollView}>
             <SearchBar onSearch={onSearch} onFocus={onFocus} onBlur={onBlur} />
-            {!isSearch && <>
+            {!isSearch ? <>
                 <Text style={textStyles.title}>Welcome {!isGuest ? username : "Guest"}</Text>
                 <MainPageBanner />
                 <GameListCard title={"Popular Games"} />
@@ -45,6 +97,11 @@ export default MainScreenComponents = () => {
                 <ReviewListCard title={"Friend Reviews"} />
                 <MoreGamesGrid />
                 <CustomButton title="Logout" onPress={onLogout} />
+            </>
+            :<>
+                {searchFetch && searchFetch.games.map((game) => (
+                    <TextButton key={game.game_slug} title={game.gameLabel} onPress={() => navigation.navigate('Game', { gameId: game.game_slug })} />
+                ))}
             </>}
         </ScrollView>
     )
