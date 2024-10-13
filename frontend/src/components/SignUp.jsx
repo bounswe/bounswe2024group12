559593408fd15b388fbca943ef62d4dad279053e
Isil Su Karakuzu <isilsukarakuzu1967@gmail.com>
2024-10-13 16:31:13 +0300

    feat: added submission handling

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -44,119 +44,144 @@ const SignUp = () => {
     setErrMsg("");
   }, [email, password, confirmPassword]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const v1 = EMAIL_REGEX.test(email);
+    const v2 = PWD_REGEX.test(password);
+    const v3 = password === confirmPassword;
+    if (!(v1 && v2 && v3)) {
+      setErrMsg("Please fix the errors in the form.");
+      return;
+    }
+    setSuccess(true);
+  }
+
   return (
-    <section>
-      <p ref={errRef} className={errMsg ? "error" : "hidden"}> {errMsg} </p>
-      <h1>Sign Up</h1>
-      <form>
-        <label htmlFor="email">
-          Email
-          <span>
-            {validEmail ? ( <FontAwesomeIcon icon={faCheck} /> ) : null}
-          </span>
-          <span>
-            {emailFocus && !validEmail ? ( <FontAwesomeIcon icon={faTimes} /> ) : null}
-          </span>
-        </label>
-          <input
-            type="text"
-            id="email"
-            ref={emailRef}
-            autoComplete="off"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            aria-invalid={validEmail ? "false" : "true"}
-            aria-describedby="email-error"
-            onFocus={() => setEmailFocus(true)}
-            onBlur={() => setEmailFocus(false)}
-          />
-          <p id="email-error">
-            {emailFocus && !validEmail ? "Please enter a valid email address." : ""}
+    <>
+      {success ? (
+        <section>
+          <h1>Success!</h1>
+          <p>Your account has been created.</p>
+          <p>
+            <a href="/login">Log in</a>
           </p>
+        </section>) : (
+        <section>
+          <p ref={errRef} className={errMsg ? "error" : "hidden"}> {errMsg} </p>
+          <h1>Sign Up</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              Email
+              <span>
+                {validEmail ? (<FontAwesomeIcon icon={faCheck} />) : null}
+              </span>
+              <span>
+                {emailFocus && !validEmail ? (<FontAwesomeIcon icon={faTimes} />) : null}
+              </span>
+            </label>
+            <input
+              type="text"
+              id="email"
+              ref={emailRef}
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              aria-invalid={validEmail ? "false" : "true"}
+              aria-describedby="email-error"
+              onFocus={() => setEmailFocus(true)}
+              onBlur={() => setEmailFocus(false)}
+            />
+            <p id="email-error">
+              {emailFocus && !validEmail ? "Please enter a valid email address." : ""}
+            </p>
 
-        <label htmlFor="name">
-          Name
-          <span>
-            {nameFocus ? ( <FontAwesomeIcon icon={faCheck} /> ) : null}
-          </span>
-        </label>
-          <input
-            type="text"
-            id="name"
-            autoComplete="off"
-            onChange={(e) => setName(e.target.value)}
-            onFocus={() => setNameFocus(true)}
-            onBlur={() => setNameFocus(false)}
-          />
+            <label htmlFor="name">
+              Name
+              <span>
+                {nameFocus ? (<FontAwesomeIcon icon={faCheck} />) : null}
+              </span>
+            </label>
+            <input
+              type="text"
+              id="name"
+              autoComplete="off"
+              onChange={(e) => setName(e.target.value)}
+              onFocus={() => setNameFocus(true)}
+              onBlur={() => setNameFocus(false)}
+            />
 
-        <label htmlFor="password">
-          Password
-          <span>
-            {validPassword ? ( <FontAwesomeIcon icon={faCheck} /> ) : null}
-          </span>
-          <span>
-            {passwordFocus && !validPassword ? ( <FontAwesomeIcon icon={faTimes} /> ) : null}
-          </span>
-        </label>
-          <input
-            type="password"
-            id="password"
-            autoComplete="off"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            aria-invalid={validPassword ? "false" : "true"}
-            aria-describedby="password-error"
-            onFocus={() => setPasswordFocus(true)}
-            onBlur={() => setPasswordFocus(false)}
-          />
-          <p id="password-error">
-            {passwordFocus && !validPassword ? "Password must be at least 8 characters long and contain at least one letter and one number." : ""}
+            <label htmlFor="password">
+              Password
+              <span>
+                {validPassword ? (<FontAwesomeIcon icon={faCheck} />) : null}
+              </span>
+              <span>
+                {passwordFocus && !validPassword ? (<FontAwesomeIcon icon={faTimes} />) : null}
+              </span>
+            </label>
+            <input
+              type="password"
+              id="password"
+              autoComplete="off"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              aria-invalid={validPassword ? "false" : "true"}
+              aria-describedby="password-error"
+              onFocus={() => setPasswordFocus(true)}
+              onBlur={() => setPasswordFocus(false)}
+            />
+            <p id="password-error">
+              {passwordFocus && !validPassword ? "Password must be at least 8 characters long and contain at least one letter and one number." : ""}
+            </p>
+
+            <label htmlFor="confirm-password">
+              Confirm Password
+              <span>
+                {(validConfirmPassword && password) ? (<FontAwesomeIcon icon={faCheck} />) : null}
+              </span>
+              <span>
+                {confirmPasswordFocus && !validConfirmPassword ? (<FontAwesomeIcon icon={faTimes} />) : null}
+              </span>
+            </label>
+
+            <input
+              type="password"
+              id="confirm-password"
+              autoComplete="off"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              aria-invalid={validConfirmPassword ? "false" : "true"}
+              aria-describedby="confirm-password-error"
+              onFocus={() => setConfirmPasswordFocus(true)}
+              onBlur={() => setConfirmPasswordFocus(false)}
+            />
+            <p id="confirm-password-error">
+              {confirmPasswordFocus && !validConfirmPassword ? "Passwords do not match." : ""}
+            </p>
+
+            <button
+              type="submit"
+              disabled={!(validEmail && validPassword && validConfirmPassword)}
+              onClick={(e) => {
+                e.preventDefault();
+                if (validEmail && validPassword && validConfirmPassword) {
+                  setSuccess(true);
+                } else {
+                  setErrMsg("Please fix the errors in the form.");
+                }
+              }}
+            >
+              Sign Up
+            </button>
+          </form>
+          <p>
+            Already have an account? <a href="/login">Log in</a>
           </p>
-
-        <label htmlFor="confirm-password">
-          Confirm Password
-          <span>
-            {(validConfirmPassword && password)? ( <FontAwesomeIcon icon={faCheck} /> ) : null}
-          </span>
-          <span>
-            {confirmPasswordFocus && !validConfirmPassword ? ( <FontAwesomeIcon icon={faTimes} /> ) : null}
-          </span>
-        </label>
-
-          <input
-            type="password"
-            id="confirm-password"
-            autoComplete="off"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-            aria-invalid={validConfirmPassword ? "false" : "true"}
-            aria-describedby="confirm-password-error"
-            onFocus={() => setConfirmPasswordFocus(true)}
-            onBlur={() => setConfirmPasswordFocus(false)}
-          />
-          <p id="confirm-password-error">
-            {confirmPasswordFocus && !validConfirmPassword ? "Passwords do not match." : ""}
+          <p>
+            Continue as a guest? <a href="/home">Continue</a>
           </p>
-
-        <button
-          type="submit"
-          disabled={!(validEmail && validPassword && validConfirmPassword)}
-          onClick={(e) => {
-            e.preventDefault();
-            if (validEmail && validPassword && validConfirmPassword) {
-              setSuccess(true);
-            } else {
-              setErrMsg("Please fix the errors in the form.");
-            }
-          }}
-        >
-          Sign Up
-        </button>
-      </form>
-      <p>
-        Already have an account? <a href="/login">Log in</a>
-      </p>
-    </section>
+        </section>)}
+    </>
   )
 }
 
