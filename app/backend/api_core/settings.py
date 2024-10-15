703646c395a2bdf93b8805d0570b3b875d203cee
Soner Kuyar <81442313+sonerkuyar@users.noreply.github.com>
2024-10-15 19:15:12 +0300

    Deployment Config Added

--- app/backend/api_core/settings.py
+++ app/backend/api_core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c_wp0u5-_d*&-+y%xzg0gsv1_oyo=l0)#5_()bf*)(#^y@zeq$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.99.133.190']
 
 
 # Application definition
