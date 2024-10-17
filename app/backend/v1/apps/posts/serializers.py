b125de3a063d8ab599009298a6edc5f7ad0c9679
yusufaygun <yusufaygun2905@icloud.com>
2024-10-17 16:45:58 +0300

    Media functionality corrected
    - initialized a custom user model
    - added a volume to keep media files in docker
    - corrected post model and added user field to posts

--- app/backend/v1/apps/posts/serializers.py
+++ app/backend/v1/apps/posts/serializers.py
@@ -1,15 +1,12 @@
 from rest_framework import serializers
 from .models import Post
 
-# TODO: Add user to the posts after creating user model
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        #fields = ['id', 'post_image', 'fen', 'post_text', 'user', 'created_at']
-        fields = ['id', 'post_image', 'fen', 'post_text', 'created_at']
-        #read_only_fields = ['id', 'user', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ['id', 'post_image', 'fen', 'post_text', 'user', 'created_at']
+        read_only_fields = ['id', 'user', 'created_at']
     
     def create(self, validated_data):
         return Post.objects.create(**validated_data)
