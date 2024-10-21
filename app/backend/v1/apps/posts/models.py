882622a033211bea670d1fc5c73b8e3e2cae7b4f
Soner Kuyar <81442313+sonerkuyar@users.noreply.github.com>
2024-10-21 15:59:14 +0300

    Revert "Post app functionality completed"
    This reverts commit d8fbd1ca44a545868d65b9e632348b7f0ed09165.

--- app/backend/v1/apps/posts/models.py
+++ app/backend/v1/apps/posts/models.py
@@ -3,19 +3,13 @@ from django.db import models
 
 from v1.apps.accounts.models import CustomUser
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
+# TODO: post image size and format check may be implemented here
 
 class Post(models.Model):
     post_image = models.ImageField(upload_to='post_images/', null=True, blank=True)
-    title = models.CharField(max_length=255)
     fen = models.CharField(max_length=255, null=True, blank=True)
     post_text = models.TextField(null=True, blank=True)
-    tags = models.ManyToManyField(Tag, related_name='posts')  # Tag relation
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts', default=1)  # added user 1 as default
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
--- app/backend/v1/apps/posts/serializers.py
+++ app/backend/v1/apps/posts/serializers.py
@@ -1,50 +1,12 @@
-import base64
-from django.core.files.base import ContentFile
 from rest_framework import serializers
-from .models import Post, Tag
+from .models import Post
 
-# Special field for converting image data into base64 format
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        # If data is not in base64 string format, apply normal procedure
-        if isinstance(data, str) and data.startswith('data:image'):
-            # Solve the data that comes in base64 format
-            format, imgstr = data.split(';base64,')  # split the format (data:image/png) part and base64 part
-            ext = format.split('/')[-1]  # Find the file extension (eg. png)
-            data = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')  # Create a temp file
-        return super().to_internal_value(data)
-
-    def to_representation(self, value):
-        # Convert the data into base64 format
-        if value:
-            with open(value.path, 'rb') as image_file:
-                return 'data:image/{};base64,{}'.format(value.file.name.split('.')[-1], base64.b64encode(image_file.read()).decode())
-        return None
-    
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ['name']
 
 class PostSerializer(serializers.ModelSerializer):
-    post_image = Base64ImageField(required=False, allow_null=True)  # Base64 image field
-    user = serializers.SerializerMethodField() # User
-    tags = TagSerializer(many=True)  # Tag list
-
     class Meta:
         model = Post
-        fields = ['id', 'post_image', 'title', 'fen', 'post_text', 'tags', 'user', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ['id', 'post_image', 'fen', 'post_text', 'user', 'created_at']
+        read_only_fields = ['id', 'user', 'created_at']
     
     def create(self, validated_data):
-        tags_data = validated_data.pop('tags')  # Get tag data
-        post = Post.objects.create(**validated_data)
-        for tag_data in tags_data:
-            tag, created = Tag.objects.get_or_create(name=tag_data['name'])  # Get tags or create new one
-            post.tags.add(tag)
-        return post
-        #return Post.objects.create(**validated_data)
-    
-    def get_user(self, obj):
-        return obj.user.username
-    
+        return Post.objects.create(**validated_data)
