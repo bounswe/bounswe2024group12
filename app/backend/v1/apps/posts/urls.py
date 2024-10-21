2b1ed378f6a63a7f6386199574a05ebe66f35f3a
Soner Kuyar <81442313+sonerkuyar@users.noreply.github.com>
2024-10-21 17:32:23 +0300

    Adding List posts url

--- app/backend/v1/apps/posts/urls.py
+++ app/backend/v1/apps/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create_post, name='create-post'),  # Create a post
     path('<int:post_id>/', views.get_post, name='get-post'),  # Get a post by ID
+    path('list_posts/', views.list_posts, name='list-posts'),  # List all posts
 ]
