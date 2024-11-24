022f337040196451c9c538938131d229af05e1aa
451 group 12 <group12@example.com>
2024-11-24 20:09:41 +0000

    drf-yasg files are added

--- app/backend/api_core/settings.py
+++ app/backend/api_core/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 # Absolute path where collected static files will be stored.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
